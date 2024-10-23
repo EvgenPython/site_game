d8b55c6b55ac0e3c47de82ecdc5b36d8eb8ad50e
keba_e <fsdfhsdfh@fjsdfjsd.com>
2024-10-23 17:54:14 +0300

    V 1.4

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-a = 1
-b = 100
-
-for i in range(a, b+1):
-    for j in range(2, i):
-        if i % j == 0:
-            break
-    else:
-        print(i, end=" ")
+# Підрахувати кількість цілих
+# чисел у діапазоні від 100 до 999, у яких є дві однакові цифри.
 
 
-for i in range(a, b+1):
-    for j in range(2, i):
-        if i % j == 0:
-            break
-    else:
-        print(i, end=" ")
+count = 0
+for i in range(100, 1000): # 100 101 102 103
+    # i = 100
+    a = i // 100
+    b = i // 10 % 10
+    c = i % 10
+    if a == b:
+        count += 1
+    elif b == c:
+        count += 1
+    elif a == c:
+        count += 1
+
+print(count)
+
+# print(345 // 100)
+# print(345 // 10 % 10)
+# print(345 % 10)
