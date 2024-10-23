df807887c4b149e50d6857648ad5e6a9a0dd46d1
keba_e <fsdfhsdfh@fjsdfjsd.com>
2024-10-23 18:34:30 +0300

    V 1.52

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# Підрахувати кількість цілих чисел у діапазоні
-# від 100 до 9999, у яких усі цифри різні.
-
 count = 0
 for i in range(100, 10000):
     if i < 1000: # 102
@@ -14,7 +11,11 @@ for i in range(100, 10000):
         b = i // 100 % 10
         c = i // 10 % 10
         d = i % 10
-        if a != b and a != c and a != c and b != c and b != d and c != d:
+        if a != b and a != c and a != d and b != c and b != d and c != d:
             count += 1
-
-
+print(count)
+c2 = 0
+for i in range(100, 10000):
+    if len(set(str(i))) == len(str(i)):
+        c2 += 1
+print(c2)
