37200c3591d294f164429865dd860b5dbb701c00
keba_e <fsdfhsdfh@fjsdfjsd.com>
2024-10-23 17:31:21 +0300

    V 1.3

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ for i in range(a, b+1):
             break
     else:
         print(i, end=" ")
+
+
+for i in range(a, b+1):
+    for j in range(2, i):
+        if i % j == 0:
+            break
+    else:
+        print(i, end=" ")
